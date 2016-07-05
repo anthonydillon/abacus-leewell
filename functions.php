6ac2f2b306c700413cf9e27d62e273b3dfb4cd11
Anthony Dillon <me@anthonydillon.com>
2016-07-05 23:04:59 +0100

    Add product post type

--- functions.php
+++ functions.php
@@ -1,3 +1,20 @@
 <?php
+  /**
+  * Register widget area.
+  */
+  function abacusleewell_widgets_init() {
+    register_post_type( 'product', array(
+      'labels' => array(
+        'name' => 'Products',
+        'singular_name' => 'Product',
+       ),
+      'description' => 'Abacus Leewell products',
+      'public' => true,
+      'menu_position' => 20,
+      'supports' => array( 'title', 'editor' )
+    ));
+  }
+  add_action( 'widgets_init', 'abacusleewell_widgets_init' );
+
   remove_filter( 'get_post_meta', 'wpautop' );
 ?>
