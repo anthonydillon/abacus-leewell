d3aa28569cb44a3101a16c841f6e272a36d6c892
Anthony Dillon <me@anthonydillon.com>
2017-03-09 08:54:04 +0000

    Build the takeover page

---
+++ takeover.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Takeover Template
+ *
+ *
+ * @file           takeover.php
+ * @package        Abacus Leewell
+ * @author         Anthony Diilon
+ * @version        Release: 0.2
+ */
+ /* Template Name: Takeover */
+
+ ?>
+
+ <!doctype html>
+ <html lang="en" dir="ltr">
+
+ <head>
+     <meta charset="UTF-8" />
+     <meta name="description" content="" />
+     <meta name="keywords" content="" />
+     <meta name="author" content="Anthony Dillon" />
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+     <title>Abacus Leewell</title>
+
+     <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
+
+     <!-- stylesheets -->
+     <link href='https://fonts.googleapis.com/css?family=Exo+2:400,700,300' rel='stylesheet' type='text/css'>
+     <link rel="stylesheet" type="text/css" media="screen" href="https://assets.ubuntu.com/v1/vanilla-framework-version-0.0.61.min.css" />
+     <link rel="stylesheet" type="text/css" media="screen" href="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css"/>
+     <link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_directory'); ?>/style.css" />
+     <link rel='stylesheet' id='sharedaddy-css' href='http://abacus-leewell.co.uk/wp-content/plugins/jetpack/modules/sharedaddy/sharing.css?ver=2.3.1' type='text/css' media='all' />
+
+ </head>
+ <body>
+
+    <div class="wrapper">
+        <div class="row no-border">
+            <div class="inner-wrapper">
+                <div class="eight-col prepend-two align-center">
+                    <img src="http://abacus-leewell.co.uk/wp-content/themes/abacus-leewell/img/abacus-leewell-logo.png" alt="Abacus Leewell">
+                </div>
+            </div>
+        </div>
+
+        <div class="row no-border" style="padding-top: 0; padding-bottom: 0;">
+            <div class="inner-wrapper">
+                <div class="eight-col prepend-two align-center">
+                    <p>It is with great sadness that we inform you that we have taken the painful decision to cease&nbsp;trading.
+                    <p>We have tried not to compromise your product and/or services supply and have reached agreement with carefully selected suppliers, so that all our customers can receive the same quality service and pricing they have been used&nbsp;to.
+                </div>
+            </div>
+        </div>
+
+        <div class="row no-border">
+            <div class="inner-wrapper align-center">
+                <div class="twelve-col">
+                    <h2>Please contact:</h2>
+                </div>
+                <div class="four-col box">
+                    <h3>CONSUMABLES - TONERS, INKS &amp; DRUMS &amp; STATIONERY</h3>
+                    <h4>Abacus Stationery &amp; Office Supplies</h4>
+                    <p>
+                        Contact: Mark or Wendy<br />
+                        Tel: 01462 628847<br />
+                        Fax: 01462 626542<br />
+                        Mob: 07958 462622<br />
+                        e-mail: info@abacussupplies.com
+                    </p>
+                </div>
+
+                <div class="four-col box">
+                    <h3>IT SERVICES (including PC REPAIR)</h3>
+                    <h4>ACN Solutions</h4>
+                    <p>
+                        Contact: Andy<br />
+                        Tel: 01234 480378<br />
+                        Mob: 07933 795338<br />
+                        e-mail: andy@acnsolutions.co.uk
+                    </p>
+                </div>
+
+                <div class="four-col last-col box">
+                    <h3>PRINTER REPAIRS<br /><br/></h3>
+                    <h4>MW Printer Services</h4>
+                    <p>
+                        Contact: Martin<br />
+                        Mob: 07554 287632<br />
+                        e-mail: mwprinterservices@gmail.com
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
